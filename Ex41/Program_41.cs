5e4a207875ad04062aaf72ff2ed5fcd03f7a2d80
«Nikolay <antonooff_2nic@mail.ru>
2023-01-16 13:35:29 +0800

    initial commit

---
+++ Ex41/Program_41.cs
@@ -0,0 +1,18 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2,  1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите числа (через пробел): ");
+int[] numbers = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+int NumberCountGreatZero(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) result++;
+    }
+    return result;
+}
+
+int count = NumberCountGreatZero(numbers);
+Console.WriteLine($"Чисел больше 0 ->> {count}");
---
+++ Ex43/Program_43.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double GetNumberByUser(string n)   
+{
+    Console.Write(n);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+double k1 = GetNumberByUser("Введите k1: ");
+double b1 = GetNumberByUser("Введите b1: ");
+double k2 = GetNumberByUser("Введите k2: ");
+double b2 = GetNumberByUser("Введите b2: ");
+
+
+void GetCoord(double k1, double b1, double k2, double b2)
+{
+    if (k1 == k2) Console.WriteLine("Прямые параллельны");
+    else
+    {
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k1 * (b2 - b1) / (k1 - k2) + b1;
+        System.Console.WriteLine($"Координаты перечесения двух прямых: (x: {x} , y: {y})");
+    }
+}
+
+GetCoord(k1, b1, k2, b2);
+
